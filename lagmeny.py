09358815b025188302b7d1ad333b617341878a8d
Erik Weber <terbolous@gmail.com>
2016-02-10 14:24:38 +0100

    updated lagmeny.py to parse yaml, and updated json file

--- lagmeny.py
+++ lagmeny.py
@@ -4,6 +4,7 @@ import json
 import re
 import posixpath
 import urlparse
+import yaml
 from urllib import FancyURLopener
 
 class MyOpener(FancyURLopener, object):
@@ -18,13 +19,28 @@ dayindex = { 1: 'Mandag',
              4: 'Torsdag',
              5: 'Fredag'
            }
+dayindex_rev = { 'monday': '1',
+             'tuesday': '2',
+             'wednesday': '3',
+             'thursday': '4',
+             'friday': '5'
 
-def addDay(day):
-    global meny
+}
+
+def parseYaml():
+    with open('menu.yaml') as file:
+        yaml_data = file.read()
+    yaml_dump = yaml.load(yaml_data)
+    for day, meals in yaml_dump.iteritems():
+        # day is the literal, convert to day of week number
+        dayofweek = int(dayindex_rev[day])
+        addDay(dayofweek, meals)
+
+def addDay(day, meals):
+    global menu
     day_literal = dayindex[day]
-    print("Please inform of the meals for %s:" % day_literal)
-    daysoup = raw_input("Soup: ")
-    daymeal = raw_input("Warm meal: ")
+    daymeal = meals["meal"]
+    daysoup = meals["soup"]
     menu[day] = {}
     menu[day]["name"] = day_literal
     menu[day]["soup"] = {}
@@ -35,19 +51,20 @@ def addDay(day):
     menu[day]["meal"]["picture"] = findPicture(daymeal)
 
 def findPicture(meal):
-    search_term = "https://www.google.pt/search?q=%s&biw=80&bih=80&source=lnms&tbm=isch&sa=X&tbs=isz:l&tbm=isch" % meal
+    search_term = (u"https://www.google.pt/search?q=%s&biw=80&bih=80&source=lnms&tbm=isch&sa=X&tbs=isz:l&tbm=isch" % meal).encode('utf-8')
     page = myopener.open(search_term)
     html = page.read()
     for mymatch in re.finditer(r'<a href="/imgres\?imgurl=(.*?)&amp;imgrefurl', html, re.IGNORECASE | re.DOTALL | re.MULTILINE):
         for match in re.finditer(r'(.*\.(?:jpg|png))', mymatch.group(1), re.IGNORECASE):
             path = match.group(0)
-            return path
+            path_split = path.split("://")
+            new_path = "://%s" % path_split[1]
+            return new_path
 
 def saveMenu():
     with open('menu.json', 'w') as f:
         f.write(json.dumps(menu))
 
 if __name__ == "__main__":
-    for day in range(1, 6):
-        addDay(day)
+    parseYaml()
     saveMenu()
